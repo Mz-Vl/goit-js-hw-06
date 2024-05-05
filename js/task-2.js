c6c42ed395377d43cb35b2af055ddeedb0c4322a
Vladyslav Mazur <vladopermoz@gmail.com>
2024-05-05 10:32:53 +0300

    HW 6

---
+++ js/task-2.js
@@ -0,0 +1,26 @@
+class Storage {
+  constructor(items) {
+    this.items = items;
+  }
+  getItems() {
+    return this.items;
+  }
+  addItem(newItem) {
+    this.items.push(newItem);
+  }
+  removeItem(itemToRemove) {
+    if (this.items.includes(itemToRemove)) {
+      this.items.splice(this.items.indexOf(itemToRemove), 1);
+    }
+  }
+}
+
+
+console.log("Task 2");
+const storage = new Storage(["Nanitoids", "Prolonger", "Antigravitator"]);
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator"]
+storage.addItem("Droid");
+console.log(storage.getItems()); // ["Nanitoids", "Prolonger", "Antigravitator", "Droid"]
+storage.removeItem("Prolonger");
+console.log(storage.getItems()); // ["Nanitoids", "Antigravitator", "Droid"]
+console.log("\n");
